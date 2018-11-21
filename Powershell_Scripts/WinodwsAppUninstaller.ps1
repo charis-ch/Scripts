ddf3e4a0169592b4f32f7f421035ce88f4f1b86f
charis-ch <36160732+charis-ch@users.noreply.github.com>
2018-11-21 22:46:14 +0200

    Windows app uninstaller
    Powershell script for uninstalling default windows applications such as 3d builder, xbox, sundry games etc.

---
+++ Powershell_Scripts/WinodwsAppUninstaller.ps1
@@ -0,0 +1,99 @@
+echo "Uninstalling 3dbuilder..."
+Get-AppxPackage  *3dbuilder* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Asphalt8Airborne..."
+Get-AppxPackage  *Asphalt8Airborne* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Candy Crush Soda Saga ..."
+Get-AppxPackage  *CandyCrushSodaSaga* | Remove-AppxPackage
+echo "Done!" 
+echo "Uninstalling Contact ..."
+Get-AppxPackage  *ContactSupport* | Remove-AppxPackage
+echo "Done!" 
+echo "Uninstalling Drawboard PDF ..."
+Get-AppxPackage  *DrawboardPDF* | Remove-AppxPackage
+echo "Done!" 
+echo "Uninstalling windows alarms ..."
+Get-AppxPackage  *windowsalarms* | Remove-AppxPackage
+echo "Done!" 
+echo "Uninstalling windows camera..."
+Get-AppxPackage  *windowscamera* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling FarmVille2CountryEscape ..."
+Get-AppxPackage  *FarmVille2CountryEscape* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Facebook ..."
+Get-AppxPackage  *Facebook* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling FeedbackHub ..."
+Get-AppxPackage  *WindowsFeedbackHub* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling windows camera..."
+Get-AppxPackage  *windowscamera* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Get Office ..."
+Get-AppxPackage  *officehub* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Get Started ..."
+Get-AppxPackage  *getstarted* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Groove Music ..."
+Get-AppxPackage  *zunemusic* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Windows Maps  ..."
+Get-AppxPackage  *windowsmaps* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Windows Maps  ..."
+Get-AppxPackage  *A278AB0D.MarchofEmpires* | Remove-AppxPackage
+echo "Done!"
+
+echo "Uninstalling Minecraft  ..."
+Get-AppxPackage  *MinecraftUWP* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Money ..."
+Get-AppxPackage  *bingfinance* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling  Paid Wi-Fi and Cellular ..."
+Get-AppxPackage   *OneConnect* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling  OneNote ..."
+Get-AppxPackage  *onenote* | remove-appxpackage
+echo "Done!"
+echo "Uninstalling Sway  ..."
+Get-AppxPackage  *sway* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Solitaire  ..."
+Get-AppxPackage  *solitairecollection* | Remove-AppxPackage
+echo "Done!"
+
+echo "Uninstalling Movies & TV   ..."
+Get-AppxPackage  *zunevideo* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Netflix   ..."
+Get-AppxPackage  *Netflix* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling News ..."
+Get-AppxPackage  *bingnews* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling People ..."
+Get-AppxPackage  *people* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling  Phone Companion ..."
+Get-AppxPackage  *windowsphone* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Photos ..."
+Get-AppxPackage  *photos* | Remove-AppxPackage
+echo "Done!"
+
+echo "Uninstalling Sports  ..."
+Get-AppxPackage  *bingsports* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Weather  ..."
+Get-AppxPackage  *bingweather* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling Weather ..."
+Get-AppxPackage  *Twitter* | Remove-AppxPackage
+echo "Done!"
+echo "Uninstalling  Xbox ..."
+Get-AppxPackage  *xboxapp* | Remove-AppxPackage
+echo "Done!"
