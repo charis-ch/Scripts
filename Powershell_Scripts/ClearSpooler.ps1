5e4c01d965e8ae803c303261a934b5c147bee959
charis-ch <36160732+charis-ch@users.noreply.github.com>
2022-05-27 13:37:32 +0300

    Clear spooler script

---
+++ Powershell_Scripts/ClearSpooler.ps1
@@ -0,0 +1,41 @@
+﻿Function Check-RunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+ 
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       #Exit from the current, unelevated, process
+       Exit
+ 
+    }
+}
+ 
+#Check Script is running with Elevated Privileges
+Check-RunAsAdministrator
+
+
+#Read more: https://www.sharepointdiary.com/2015/01/run-powershell-script-as-administrator-automatically.html#ixzz7UTCRJfAr
+cd "C:\Windows\System32"
+net stop spooler
+start .\spool\PRINTERS\
+Get-ChildItem C:\Windows\System32\spool\PRINTERS -Include *.* -Recurse | ForEach  { $_.Delete()}
+net start spooler
+Read-Host -Prompt "Press any key to continue"
